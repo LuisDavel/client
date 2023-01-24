24dc245ba193fa745fcb02c21eb477fd7940ec80
Davel <luisdavel90@outlook.com>
2023-01-23 23:01:29 -0300

    Deploy

--- src/pages/api/animals/index.ts
+++ src/pages/api/animals/index.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../../lib/prisma';
 import { getCookie, setCookie, deleteCookie } from 'cookies-next';
+import { celebrate, Joi } from 'celebrate';
+import next from 'next';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,9 +12,26 @@ export default async function handler(
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
+
+  const token = getCookie('@localize:idUser', { req, res });
+  try {
+    celebrate({
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        type: Joi.string().required(),
+        image: Joi.string().required(),
+        dt_born: Joi.date().required(),
+        race: Joi.string().required(),
+        email: Joi.string().required(),
+        size: Joi.string().required(),
+        temperament: Joi.string().required()
+      })
+    })(req, res);
+  } catch (err) {
+    return res.status(400).json({ message: 'Invalid body' });
+  }
   const { name, type, image, dt_born, race, email, size, temperament } =
     req.body;
-  const token = getCookie('@localize:idUser', { req, res });
   console.log(email);
   const animal = await prisma.animal.create({
     data: {
